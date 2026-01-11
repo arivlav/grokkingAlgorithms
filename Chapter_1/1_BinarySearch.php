86ffe32d6adbda45e2730c7a3c399601cc0ef9ab
arivlav <arivlav@list.ru>
2026-01-11 10:30:45 +0300

    заметки про бинарный поиск

---
+++ Chapter_1/1_BinarySearch.php
@@ -0,0 +1,28 @@
+<?php
+
+function binarySearch(array $list, mixed $item): int|float|string|null {
+    $low = 0;
+    $high = count($list) - 1;
+
+    while ($low <= $high) {
+        $mid = floor(($low + $high) / 2);
+        $guess = $list[$mid];
+
+        if ($guess == $item) {
+            return $mid;
+        }
+
+        if ($guess > $item) {
+            $high = $mid - 1;
+        } else {
+            $low = $mid + 1;
+        }
+    }
+
+    return null;
+}
+
+$myList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+echo binarySearch($myList, 3);
+echo binarySearch($myList, -1);
